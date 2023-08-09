4936d4cfdb2f3918e0eed59a3127012e3c1d7185
luttfimohamad <luttfimohamad@gmail.com>
2023-08-09 13:12:13 +0700

    added function to delete product

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -3,9 +3,13 @@ import Header from '@/components/header'
 import Link from 'next/link'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import DeleteConfirmation from '@/components/DeleteConfirmation'
 
 export default function Products() {
     const [products,setProducts] = useState([]);
+    const [showConfirmation, setShowConfirmation] = useState(false); // state untuk menampilkan atau menyembunyikan popup konfirmasi penghapusan produk
+    const [productToDelete, setProductToDelete] = useState(null); // state untuk menyimpan data produk yang akan dihapus
+
     const fetchProducts = async () => {
       try {
         const response = await axios.get('https://dummyjson.com/products',{
@@ -17,6 +21,33 @@ export default function Products() {
       } catch (err) {}
     };
   
+    // fungsi showDeleteConfirmation akan menerima satu parameter, yaitu data produk yang akan dihapus.
+    const showDeleteConfirmation = (product) => {
+      setProductToDelete(product); // menyimpan data produk yang akan dihapus ke state productToDelete
+      setShowConfirmation(true); // menampilkan popup konfirmasi penghapusan produk
+    };
+  
+    const hideDeleteConfirmation = () => {
+      setShowConfirmation(false); // menyembunyikan popup konfirmasi penghapusan produk
+      setProductToDelete(null); // menghapus data produk yang akan dihapus dari state productToDelete
+    };
+  
+    // fungsi deleteProduct akan menerima satu parameter, yaitu id produk yang akan dihapus.
+    const deleteProduct = async (id) => {
+      try {
+        // mengirim request ke API untuk menghapus produk
+        const response = await axios.delete(`https://dummyjson.com/products/${id}`);
+        if (response.data.isDeleted) {
+          // jika produk berhasil dihapus, maka kita akan menghapus produk dari state products
+          setProducts((prevProducts) => prevProducts.filter((product) => product.id !== id));
+          // menyembunyikan modal konfirmasi
+          hideDeleteConfirmation();
+        }
+      } catch (err) {
+        console.error('Error deleting product:', err);
+      }
+    };
+
     useEffect(()=> {
       fetchProducts();
     }, []);
@@ -33,18 +64,40 @@ export default function Products() {
           {(() => {
             if (products && products.length >0) {
               return products.map((item) => (
-                  <div className='bg-slate-800 p-3 rounded-lg flex-col justify-center items-center w-96 mb-5 relative lg:w-80'>
+                  <div key={item.id} className='bg-slate-800 p-3 rounded-lg flex-col justify-center items-center w-96 mb-5 relative lg:w-80'>
                       <img src={item['thumbnail']} alt="" className='h-[200px] w-[400] rounded-lg mb-3 ml-1 mt-2' width={400} height={200}/>
                       <h1 className='text-lg font-medium ml-1'>{item['title']}</h1>
                       <h1 className='text-m font-medium ml-1'>Rp {item['price']}</h1>
                       <div className='flex justify-between mt-5 ml-1'>
-                        <Link href='/'><button className=' bg-white px-4 py-2 border-4 border-red-500  hover:bg-red-500 hover:text-white font-bold rounded-lg mr-5 text-red-500'>Remove</button></Link>
+                        <button
+                          onClick={() => showDeleteConfirmation(item)} // menampilkan popup konfirmasi penghapusan produk
+                          className='bg-white px-4 py-2 border-4 border-red-500 hover:bg-red-500 hover:text-white font-bold rounded-lg mr-5 text-red-500'
+                        >
+                          Remove
+                        </button>
                         <Link href={'/products/'+ item['id']}><button className=' bg-white px-4 py-2 border-4 border-emerald-600  hover:bg-emerald-600 hover:text-white font-bold rounded-lg mr-2 text-emerald-600'>Edit</button></Link>
                       </div>
                   </div>
               ))}
+              else {
+                return (
+                  <div className='flex justify-center items-center w-full'>
+                    <h1 className='text-xl font-bold'>No Products Found ...</h1>
+                  </div>
+                )
+              }
           }) ()}
           </div>
+          {/* menampilkan modal konfirmasi jika showConfirmation bernilai true */}
+          {showConfirmation && (
+            <div className='fixed top-0 left-0 z-[80] w-full h-screen bg-slate-600/50 backdrop-blur-sm transition-all delay-200 flex justify-center items-center'>
+              <DeleteConfirmation
+                productName={productToDelete.title} // mengambil nama produk yang akan dihapus
+                onConfirm={() => deleteProduct(productToDelete.id)} // menghapus produk
+                onCancel={hideDeleteConfirmation} // menyembunyikan modal konfirmasi
+              />
+            </div>
+          )}
         </div>
       </main>
     )
